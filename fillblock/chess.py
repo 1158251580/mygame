66ffa0f35d4d3c0da2a2b02e274e64790e768f17
Sean_TS_Wang <Sean_TS_Wang@wistron.com>
2023-07-19 09:34:10 +0800

    feat(chess.py)；add chess class

--- fillblock/chess.py
+++ fillblock/chess.py
@@ -1,6 +1,32 @@
 import pygame
+import random
 from block import Block
 from collections import deque
+
+class Chess(Block):
+    def set_role(self, exp, hp, attack, defense):
+        self.exp = exp
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+    
+    def attack_enemy(self, enemy):
+        # 进行攻击计算
+        damage = self.attack - enemy.defense
+        enemy.hp -= damage
+        if enemy.hp:
+            self.level_up()
+        else:
+            self.level_up(3)
+    
+    def level_up(self,num=1):
+        # 经验值增加，触发升级条件
+        self.exp += num
+        if self.exp % 10 == 0:
+            self.hp += random.randint(0,10)
+            self.attack += random.randint(0,5)
+            self.defense += random.randint(0,5)
+
 
 class Dogface(Block):
 
--- new/chess.py
+++ new/chess.py
@@ -1,8 +1,34 @@
 import pygame
+import random
 from block import Block
 from collections import deque
 
-class Dogface(Block):
+class Chess(Block):
+    def set_role(self, exp, hp, attack, defense):
+        self.exp = exp
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+    
+    def attack_enemy(self, enemy):
+        # 进行攻击计算
+        damage = self.attack - enemy.defense
+        enemy.hp -= damage
+        if enemy.hp:
+            self.level_up()
+        else:
+            self.level_up(3)
+    
+    def level_up(self,num=1):
+        # 经验值增加，触发升级条件
+        self.exp += num
+        if self.exp // 10:
+            self.exp %= 10 
+            self.hp += random.randint(0,10)
+            self.attack += random.randint(0,5)
+            self.defense += random.randint(0,5)
+
+class Dogface(Chess):
 
     def set_role(self):
         self.role = pygame.image.load('../images/man/士兵.png')
@@ -10,7 +36,7 @@ class Dogface(Block):
     
     
 
-class Devil(Block):
+class Devil(Chess):
         
     def set_role(self):
         self.role = pygame.image.load('../images/man/boss.png')
